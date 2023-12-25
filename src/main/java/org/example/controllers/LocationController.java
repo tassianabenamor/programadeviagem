78f6c54ae0501455a86f85d8b4bd118e9413f4bf
tassianabenamor <tassiana.benamor@gmail.com>
2023-12-25 19:54:14 -0300

    .

--- src/main/java/org/example/controllers/LocationController.java
+++ src/main/java/org/example/controllers/LocationController.java
@@ -18,10 +18,31 @@ public class LocationController {
             return "Detalhes da localização com ID: " + id;
         });
 
+        // GET: Buscar localizações pelo nome
+        get("/locations/nome/:nome", (req, res) -> {
+            String nome = req.params(":nome");
+            // Implementar lógica para retornar localizações pelo nome
+            return "Localizações com o nome: " + nome;
+        });
+
+        // GET: Buscar localizações por categoria
+        get("/locations/categoria/:categoria", (req, res) -> {
+            String categoria = req.params(":categoria");
+            // Implementar lógica para retornar localizações por categoria
+            return "Localizações na categoria: " + categoria;
+        });
+
         post("/locations", (req, res) -> {
             String body = req.body();
             // Implementar lógica para criar uma nova localização com o corpo da requisição
             return "Nova localização criada com os dados: " + body;
+        });
+
+        // POST: Criar um novo evento em uma localização
+        post("/locations/eventos", (req, res) -> {
+            String body = req.body();
+            // Implementar lógica para criar um novo evento com o corpo da requisição
+            return "Novo evento criado com os dados: " + body;
         });
 
         put("/locations/:id", (req, res) -> {
@@ -31,10 +52,25 @@ public class LocationController {
             return "Localização com ID " + id + " atualizada com os dados: " + body;
         });
 
+        // PUT: Atualizar os horários de uma localização
+        put("/locations/:id/horarios", (req, res) -> {
+            String id = req.params(":id");
+            String body = req.body();
+            // Implementar lógica para atualizar os horários da localização com ID
+            return "Horários da localização com ID " + id + " atualizados com: " + body;
+        });
+
         delete("/locations/:id", (req, res) -> {
             String id = req.params(":id");
             // Implementar lógica para deletar uma localização com o ID especificado
             return "Localização com ID " + id + " deletada";
+        });
+
+        // DELETE: Deletar um evento de uma localização
+        delete("/locations/eventos/:id", (req, res) -> {
+            String id = req.params(":id");
+            // Implementar lógica para deletar um evento da localização com ID
+            return "Evento com ID " + id + " deletado";
         });
     }
 
